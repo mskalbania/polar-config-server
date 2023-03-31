851daa2a5b69585f7529c19daffcbf4ad6b978b8
mskalbania <mateusz.skalbania1@tesco.com>
2023-03-31 16:44:26 +0200

    Implementation of config server

---
+++ src/test/java/edu/polar/polarconfigserver/PolarConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.polar.polarconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
